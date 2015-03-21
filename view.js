364be0e03ca98e781bf2f4760bd05d7a89937f7a
moritzbe <moritzberthold@hotmail.com>
2015-03-21 17:51:28 +0100

    mapview

--- view.js
+++ view.js
@@ -2,7 +2,8 @@ var map;
 function initialize() {
   var mapOptions = {
     zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
+    center: new google.maps.LatLng(-34.397, 150.644),
+    scrollwheel: false,
   };
     map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
